62d124ba733e7f31098476b0bc82bb40663a66c4
unknown <YY103@ccs.local>
2024-05-25 07:42:45 +0800

    ADD TAB OF COURSES & ANNOUNCEMENT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,16 @@ import { Role } from './_models';
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const adminModule = () => import('./admin/admin.module').then(x => x.AdminModule);
 const profileModule = () => import('./profile/profile.module').then(x => x.ProfileModule);
+const CourseManagementModule = () => import('./course-management/course-management.module').then(x => x.CourseManagementModule);
+const AnnouncementFormModule = () => import('./announcement-form/announcement-form.module').then(x => x.AnnouncementFormModule);
+
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'account', loadChildren: accountModule },
     { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
+    { path: 'course-management', loadChildren: CourseManagementModule, canActivate: [AuthGuard] },
+    { path: 'announcement-form', loadChildren: AnnouncementFormModule, canActivate: [AuthGuard] },
     { path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
 
     // otherwise redirect to home
